fe38a458655d89c63bb110f13965c019a9943684
Vladyslav Okhrimenko <vladyslav.okhrimenko@signet.tv>
2022-06-20 00:26:47 +0300

    add a new state for button

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
+import { StoreModule } from '@ngrx/store';
 
 import { RegisterComponent } from './components/register/register.component';
+import { reducers } from './store/reducers';
 
 const routes: Routes = [
   {
@@ -13,7 +15,12 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [CommonModule, RouterModule.forChild(routes), ReactiveFormsModule],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+    ReactiveFormsModule,
+    StoreModule.forFeature('auth', reducers),
+  ],
   declarations: [RegisterComponent],
 })
 export class AuthModule {}
