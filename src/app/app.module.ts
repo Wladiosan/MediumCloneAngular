f8f6ee674b7935299994f4158f3ffa1a8eb52e5b
Vladyslav Okhrimenko <vladyslav.okhrimenko@signet.tv>
2022-06-19 23:58:34 +0300

    add devTools redux for angular store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { StoreModule } from '@ngrx/store';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { AuthModule } from './auth/auth.module';
-import { StoreModule } from '@ngrx/store';
+
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
@@ -13,6 +17,10 @@ import { StoreModule } from '@ngrx/store';
     AppRoutingModule,
     AuthModule,
     StoreModule.forRoot({}),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,
+      logOnly: environment.production,
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent],
